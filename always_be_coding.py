ca4c19da592f132214f3d35ab96c5c7ede949d28
Alethea Power <10975066+aletheap@users.noreply.github.com>
2023-09-23 21:45:05 -0700

    i simplify

--- always_be_coding.py
+++ always_be_coding.py
@@ -9,56 +9,42 @@ import subprocess
 import sys
 import time
 
-formatter = logging.Formatter("%(asctime)s: %(message)s")
-handler = logging.StreamHandler(sys.stdout)
-handler.setFormatter(formatter)
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-logger.addHandler(handler)
-
 VICTIM_FILE = "killer_feature.py"
+MIN_COMPILE_TIME = 2 * 60 * 60
+MAX_COMPILE_TIME = 6 * 60 * 60
 
 
 def be_coding():
-    my_dir = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(my_dir)
-    logger.debug(f"{my_dir=!r}")
+    # always be in the right directory
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
     with open(VICTIM_FILE, "w") as f:
         f.write("#!/usr/bin/env python\n" + "\n" + f"print({random.randint(0, 100)})\n")
 
-    commit_string = f'burning the {time.strftime("%I:%M:%S %p")} oil'
+    sword_fight_while_code_compiles()
 
-    logger.debug("Hey! Get back to work!")
-    # always be sword fighting
-    sword_fight_while_code_compiles(error_code=303)
-
-    logger.debug("Committed. Whew!")
     subprocess.run(["git", "add", VICTIM_FILE])
+    commit_string = f'burning the {time.strftime("%I:%M:%S %p")} oil'
     subprocess.run(["git", "commit", "-m", commit_string])
     subprocess.run(["git", "push"])
 
 
-def sword_fight_while_code_compiles(error_code):
-    logger.debug(f"I'm Compiling!")
+def sword_fight_while_code_compiles():
+    print(f"Sword fighting while code compiles...", flush=True)
 
-    assert error_code == 303, "Compile error!"
-
-    min_hours = 2
-    max_hours = 6
-    end = time.time() + random.randint(min_hours * 60 * 60, max_hours * 60 * 60)
+    end = time.time() + random.randint(MIN_COMPILE_TIME, MAX_COMPILE_TIME)
     while time.time() < end:
         wait = int(end - time.time())
         h = wait // (60 * 60)
         m = (wait - (h * 60 * 60)) // 60
         s = wait - (h * 60 * 60) - (m * 60)
-        print(f"{h:>01}:{m:>02}:{s:>02}", end="\r")
+        print(f"{h:>01}:{m:>02}:{s:>02}", end="\r", flush=True)
         time.sleep(1)
-        break
+
+    print("00:00:00 - Done!", flush=True)
 
 
 always = True
 
 while always:
     be_coding()
-    break
